62362ef9e8512728d0fa03d9cb7b3e1de27ca202
terminalgabut <trasipahit02@gmail.com>
2026-04-27 19:02:00 +0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -2,28 +2,38 @@ import layoutView from './layoutView.js';
 import Header from './header.js';
 import Navigation from './nav.js';
 import Sidebar from './sidebarView.js';
+import InputModal from './inputModal.js';
 
 export default {
     ...layoutView, // Menggabungkan template HTML dari layoutView.js
     components: {
         'header-component': Header,
         'sidebar-component': Sidebar,
-        'nav-component': Navigation
+        'nav-component': Navigation,
+        'input-component': InputModal
     },
     setup() {
         const { ref } = Vue;
         
         // State untuk kontrol Sidebar di Mobile (Android)
         const isSidebarOpen = ref(false);
+        const isInputOpen = ref(false);
 
         const toggleSidebar = () => {
             isSidebarOpen.value = !isSidebarOpen.value;
             document.body.style.overflow = isSidebarOpen.value ? 'hidden' : '';
         };
+        
+        const toggleInput = () => {
+            isInputOpen.value = !isInputOpen.value;
+            document.body.style.overflow = isInputOpen.value ? 'hidden' : '';
+        };
 
         return {
             isSidebarOpen,
-            toggleSidebar
+            isInputOpen,
+            toggleSidebar,
+            toggleInput
         };
     },
     watch: {
